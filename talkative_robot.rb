532bb4005347d275f1c7363e14d8e20e6dfc574e
rgpass <rgpass@gmail.com>
2014-09-23 16:33:35 -0400

    Added troubleshooting practices

--- talkative_robot.rb
+++ talkative_robot.rb
@@ -1,16 +1,62 @@
-puts "Test message"
-puts "We're running this in the Terminal"
+require 'pry'
 
-puts "What is your name?"
-user_name = gets.chomp
+# puts "Test message"
+# puts "We're running this in the Terminal"
 
-puts "Hey #{user_name}"
-puts "How are you doing?"
+# puts "What is your name?"
+# user_name = gets.chomp
 
-mood = gets.chomp.downcase
+# puts "Hey #{user_name}"
+# puts "How are you doing?"
 
-puts "Glad to hear you're #{mood}?"
-lyrics = "I put my thing down, flip it, and reverse it"
-puts 'Wait, did Missy just say #{lyrics}'
-puts 'I can put " quotes now'
-puts %s("now I can" do 'whatever' i want (hint hint))
+# mood = gets.chomp.downcase
+
+# puts "Glad to hear you're #{mood}?"
+
+if 5 > 6
+	puts "first response"
+else
+	puts "default response"
+end
+
+# if !(false)
+# 	puts "obvi"
+# end
+
+# unless true
+# 	puts "wait what"
+# else
+# 	puts "obvi"
+# end
+
+# if store_proximity < 1 #miles
+# 	go_to_store
+# end
+
+# go_to_store if store_proximity < 1 #miles
+# puts "Wuddup" if 5 > 4 # => Wuddup
+# puts "Testing" unless 5 > 4
+
+a = 5
+
+# case
+# when a > 5
+# 	puts "it's greater than 5"
+# when a < 5
+# 	puts "it's less than 5"
+# when a == 5
+# 	puts "it's 5!"
+# else
+# 	puts "it's not a number"
+# end
+
+case a
+when 1..5 # a == 1   a == 2   a == 3
+	  puts "It's between 1 and 5"
+when 6 # a == 6
+  puts "It's 6"
+when String  # a.class == String
+  puts "You passed a string"
+else
+  puts "You gave me #{a} -- I have no idea what to do with that."
+end
