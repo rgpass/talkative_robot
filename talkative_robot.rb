0104616f4506aa388d3c7bc652ab74aca6fc00fb
rgpass <rgpass@gmail.com>
2014-09-23 16:54:17 -0400

    Left a message for the instructor

--- talkative_robot.rb
+++ talkative_robot.rb
@@ -1,17 +1,17 @@
 require 'pry'
 
-# puts "Test message"
-# puts "We're running this in the Terminal"
+puts "Test message"
+puts "We're running this in the Terminal"
 
-# puts "What is your name?"
-# user_name = gets.chomp
+puts "What is your name?"
+user_name = gets.chomp
 
-# puts "Hey #{user_name}"
-# puts "How are you doing?"
+puts "Hey #{user_name}"
+puts "How are you doing?"
 
-# mood = gets.chomp.downcase
+mood = gets.chomp.downcase
 
-# puts "Glad to hear you're #{mood}?"
+puts "Glad to hear you're #{mood}?"
 
 if 5 > 6
 	puts "first response"
@@ -19,36 +19,38 @@ else
 	puts "default response"
 end
 
-# if !(false)
-# 	puts "obvi"
-# end
+Google: ruby ternary operator
 
-# unless true
-# 	puts "wait what"
-# else
-# 	puts "obvi"
-# end
+if !(false)
+	puts "obvi"
+end
 
-# if store_proximity < 1 #miles
-# 	go_to_store
-# end
+unless true
+	puts "wait what"
+else
+	puts "obvi"
+end
 
-# go_to_store if store_proximity < 1 #miles
-# puts "Wuddup" if 5 > 4 # => Wuddup
-# puts "Testing" unless 5 > 4
+if store_proximity < 1 #miles
+	go_to_store
+end
+
+go_to_store if store_proximity < 1 #miles
+puts "Wuddup" if 5 > 4 # => Wuddup
+puts "Testing" unless 5 > 4
 
 a = 5
 
-# case
-# when a > 5
-# 	puts "it's greater than 5"
-# when a < 5
-# 	puts "it's less than 5"
-# when a == 5
-# 	puts "it's 5!"
-# else
-# 	puts "it's not a number"
-# end
+case
+when a > 5
+	puts "it's greater than 5"
+when a < 5
+	puts "it's less than 5"
+when a == 5
+	puts "it's 5!"
+else
+	puts "it's not a number"
+end
 
 case a
 when 1..5 # a == 1   a == 2   a == 3
@@ -60,3 +62,6 @@ when String  # a.class == String
 else
   puts "You gave me #{a} -- I have no idea what to do with that."
 end
+
+
+puts "Hey Mr. Instructor dude"
