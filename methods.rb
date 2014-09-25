0188af539a10f5b60f372de33e81a7e71377f246
rgpass <rgpass@gmail.com>
2014-09-25 09:32:19 -0400

    Finished lab 3

---
+++ methods.rb
@@ -0,0 +1,54 @@
+require 'pry'
+
+# # do action
+# def print_hello
+# 	puts "hello"
+# end
+
+# # return something
+# def	square(x)
+# 	return x*x
+# end
+
+# # NO NO NO
+# def prints_goodbye_and_returns_square(x)
+# 	puts "goodbye"
+# 	x*x
+# end
+
+# # Activity
+# # Def a function whats_good that prints what's good
+# # Def a function add_five that adds 5 to whatever number is given
+# def whats_good
+# 	puts "What's good?"
+# end
+# def add_five(x)
+# 	x + 5
+# end
+# def add_five(x)
+# 	variable_given = x
+# 	answer = variable_given + 5
+# 	return answer
+# end
+
+# whats_good
+# ten_plus_five = add_five(10) # => sets ten_plus_five to 15
+# puts ten_plus_five # => "15"
+# ten_plus_five # => not doing anything
+
+# # age_message = (age < 21 ? "Too young" : "Go drink")
+
+# "Gerry".upcase
+# ["milk", "bread", "eggs"].length
+
+
+# this_message_was_printed = print_hello
+# five_squared = square(5)
+
+
+def change_age_to_fifteen
+	age = 15
+end
+change_age_to_fifteen
+puts age
+# ARGV.first
